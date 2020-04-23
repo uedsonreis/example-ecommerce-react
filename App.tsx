76df452fa28e2a35db1d6eb01504925dff2c1d06
Uedson Reis <uedsonreis@gmail.com>
2020-04-23 14:48:22 -0300

    Ajuste

--- App.tsx
+++ App.tsx
@@ -6,25 +6,13 @@ import SideMenu from './src/components/SideMenu';
 import TabNavigator from './src/components/TabNavigator';
 import LoginStack from './src/components/LoginStack';
 import SalesOrderStack from './src/components/SalesOrderStack';
-import { Ionicons } from '@expo/vector-icons';
-
-// const AppContainer: any = createAppContainer(
-//     createDrawerNavigator({
-//         Main: navigator,
-//         SalesOrder: createSalesOrderStack(),
-//         Login: createLoginStack(),
-//     }, {
-//         contentComponent: SideMenu,
-//         drawerWidth: Dimensions.get('window').width - 120
-//     })
-// );
 
 export default function App() {
     const Drawer = createDrawerNavigator();
 
     return (
         <NavigationContainer>
-            <Drawer.Navigator initialRouteName="main" drawerContent={SideMenu} >
+            <Drawer.Navigator initialRouteName="main" drawerContent={SideMenu}>
                 <Drawer.Screen name="main" component={TabNavigator} />
                 <Drawer.Screen name="salesOrder" component={SalesOrderStack} />
                 <Drawer.Screen name="login" component={LoginStack} />
